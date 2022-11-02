2256cbe6cdf7e16b8e77057154a39522a4b710b3
LamPhan <phantiensinh122@gmail.com>
2022-11-02 16:43:03 +0700

    update token
    update token

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "javascript_dart/receipt.js": "7a3b9046592eaf89d59db07aafe59b8d",
 "version.json": "e2b17b502e4b0ce0d598939bf4dcc9ec",
-"index.html": "34303a01531c2a51a64dbbcc897f504f",
-"/": "34303a01531c2a51a64dbbcc897f504f",
-"main.dart.js": "8b06b07026b9f2a557cc2c33020f799f",
+"index.html": "8cc0d192564879f0c2c3382b4fbf84d8",
+"/": "8cc0d192564879f0c2c3382b4fbf84d8",
+"main.dart.js": "ba296fd5bcf88e048603d09f51e9fd8f",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "sql-wasm.js": "7ce6f6c33529a9703e33e194e79b7691",
